4c47dfd5166110518d2a1ffb3f06d8040752b86d
FIKbol <lion59910612@mail.ru>
2025-12-14 17:09:55 +0300

    Delete FilePaths.java

--- FilePaths.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class FilePaths {
-    public static final String ACCOUNTS_FILE = "accounts.txt";
-    public static final String PRODUCTION_FILE = "production.txt";
-
-    public static final String MSG_SUCCESS = "Операция выполнена успешно.";
-    public static final String MSG_ERROR = "Ошибка: ";
-}
